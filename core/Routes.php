165e4e630867a18ebf76a8a0f66de1fd72019b71
WILLIAM FRANCISCO LEITE <william@wllsistemas.com.br>
2024-09-10 19:11:21 -0300

    Ajuste Routes

--- core/Routes.php
+++ core/Routes.php
@@ -2,15 +2,5 @@
 
 require_once 'Http.php';
 
-Http::get('/sistema/current-date', 'SistemaController@currentDate');
-
-Http::get('/config/smtp', 'SmtpController@find');
-Http::get('/cnpj/find/{cnpj}/{completa}/{atualizar}', 'CnpjController@find');
-Http::get('/release/{servico}/{versao}', 'ReleaseController@find');
-
-Http::get('/licenca/update-hd/{cnpj}/{hd}', 'LicencaController@updateHd');
-Http::get('/licenca/initial-contract/{cnpj}/{hd}', 'LicencaController@findInitial');
-Http::get('/licenca/find/{cnpj}/{hd}/{version}', 'LicencaController@find');
-
-Http::get('/cliente/find/{cnpj}', 'ClienteController@find');
-Http::put('/cliente/emissores', 'ClienteController@updateEmissores');
+Http::get('/produto/find/{id}', 'ProdutoController@find');
+Http::put('/produtos/add', 'ProdutoController@add');
