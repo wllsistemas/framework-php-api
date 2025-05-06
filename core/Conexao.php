008c8a5f9f8b59e535993bd3c797b208dbe27771
WILLIAM FRANCISCO LEITE <william@wllsistemas.com.br>
2025-05-06 18:35:16 -0300

    Melhorias e Correções
    - Adicionado porta de conexão com banco de dados na string de conexão
    - Adicionado classe Jwt no core para tratar geração e validação de token JWT
    - Correção método dispatch para aceitar métodos  com mesma assinatura mas com verbos diferentes

--- core/Conexao.php
+++ core/Conexao.php
@@ -52,7 +52,7 @@ class Conexao
                 switch (SGBD):
                     case 'mysql':
                         $opcoes = array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8');
-                        self::$pdo = new \PDO("mysql:host=" . HOST . "; dbname=" . DBNAME . ";", USER, PASSWORD, $opcoes);
+                        self::$pdo = new \PDO("mysql:host=" . HOST . "; dbname=" . DBNAME . ";port=" . PORT . ";", USER, PASSWORD, $opcoes);
                         break;
                     case 'mssql': {
                             if (SERVER == 'linux') :
--- core/Core.php
+++ core/Core.php
@@ -8,6 +8,7 @@ class Core
     public static function dispatch(array $routes)
     {
         $route_existe = false;
+        $msg_erro = '';
         $url = '/';
 
         isset($_GET['url']) && $url .= $_GET['url'];
@@ -15,53 +16,51 @@ class Core
         $url !== '/' && $url = rtrim($url, '/');
 
         foreach ($routes as $route) :
-            $regex = '#^' . preg_replace('/{(\w+)}/', '([\w\-.]+)', $route['path']) . '$#';
+            $regex = '#^' . preg_replace('/{(\w+)}/', '([\w\-.@]+)', $route['path']) . '$#';
 
             if (preg_match($regex, $url, $matches)) :
                 $route_existe = true;
                 array_shift($matches);
 
                 if ($route['method'] !== Request::method()) :
-                    Response::json([
-                        'status' => 'error',
-                        'message' => $route['method'] . ' method não aceito.'
-                    ], 405);
-                    return;
+                    $msg_erro .= 'Method ' . Request::method() . ' não aceito ou parâmetros inválidos.';
+                    continue;
                 endif;
 
                 [$controller, $action] = explode('@', $route['action']);
 
                 if (!file_exists("./controller/$controller.php")):
-                    Response::json([
-                        'status' => 'error',
-                        'message' => "Arquivo [$controller.php] não existe."
-                    ], 500);
-                    return;
+                    $msg_erro .= "Arquivo [$controller.php] não existe.";
+                    continue;
                 endif;
 
                 require_once "./controller/$controller.php";
 
                 if (!class_exists($controller)) :
-                    Response::json([
-                        'status' => 'error',
-                        'message' => "Class [$controller] não existe."
-                    ], 500);
-                    return;
+                    $msg_erro .= "Class [$controller] não existe.";
+                    continue;
                 endif;
 
                 $controller = new $controller();
 
                 if (!method_exists($controller, $action)) {
-                    Response::json([
-                        'status' => 'error',
-                        'message' => "Action [$action] não existe na class [$controller]."
-                    ], 500);
-                    return;
+                    $msg_erro .= "Action [$action] não existe na class [$controller].";
+                    continue;
                 }
 
                 $controller->$action(new Request, new Response, $matches);
+                $msg_erro = '';
+                break;
             endif;
         endforeach;
+
+        if (!empty($msg_erro)) :
+            Response::json([
+                'status' => 'error',
+                'message' => $msg_erro
+            ], 405);
+            return;
+        endif;
 
         if (!$route_existe) :
             Response::json([
